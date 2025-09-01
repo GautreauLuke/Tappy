17502868a7dfcab4b38e7203e20208a3acfb39a0
GautreauLuke <gautreauluke@gmail.com>
2025-08-31 21:55:45 -0500

    Gameplay updates
     - Added export for music
       - Music stops when game is over
     - Added space input to return to Main
     - Added restart test functionality
     - Added Game Manager Global

--- Scenes/Game/Game.cs
+++ Scenes/Game/Game.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Game : Node2D
 {
+	private static readonly PackedScene MAIN_SCENE = GD.Load<PackedScene>("res://Scenes/Main/main.tscn");
+	private static readonly PackedScene GAME_SCENE = GD.Load<PackedScene>("res://Scenes/Game/Game.tscn");
+
 
 	[Export] private Marker2D _spawnUpper;
 	[Export] private Marker2D _spawnLower;
@@ -10,6 +13,10 @@ public partial class Game : Node2D
 	[Export] private Timer _spawnTimer;
 	[Export] private PackedScene _pipesScene;
 	[Export] private Plane _plane;
+	[Export] private AudioStreamPlayer2D _music;
+	[Export] private Label _gameOverLabel;
+
+	bool IsGameOver = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -34,7 +41,9 @@ public partial class Game : Node2D
 	{
 		GD.Print("GameOver");
 		StopPipes();
-		
+		IsGameOver = true;
+		_music.Stop();
+		_gameOverLabel.Visible = true;
 	}
 
 	private void SpawnPipes()
@@ -47,6 +56,20 @@ public partial class Game : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (Input.IsActionJustPressed("fly") && IsGameOver == true)
+		{
+			GetTree().ChangeSceneToPacked(GAME_SCENE);
+		}
+
+		// if (Input.IsKeyPressed(Key.Q))
+		// {
+		// 	GetTree().ChangeSceneToPacked(MAIN_SCENE);
+		// }
+
+		if (Input.IsKeyPressed(Key.R)  && IsGameOver == true)
+		{
+			GetTree().ChangeSceneToPacked(MAIN_SCENE);
+		}
 	}
 
 	public float GetSpawnY()
