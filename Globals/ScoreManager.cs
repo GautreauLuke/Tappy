be8bc8cee8649bb16168ce3e4b9ac6bffaeabead
GautreauLuke <gautreauluke@gmail.com>
2025-09-06 22:02:09 -0500

    High Score Keeping
     - Added SaveScoreToFile
     - Added LoadScoreFromFile
     - Added functionality to write highscore =0 if file does not exist

--- Globals/ScoreManager.cs
+++ Globals/ScoreManager.cs
@@ -3,25 +3,37 @@ using System;
 
 public partial class ScoreManager : Node
 {
-    private int _score = 0;
-    private int _highScore = 0;
+    private uint _score = 0;
+    private uint _highScore = 0;
+
+    private const string SCORE_FILE = "user://tappy.save";
+
+
     public static ScoreManager Instance { get; private set; }
+
     public override void _Ready()
     {
         Instance = this;
+        LoadScoreFromFile();
     }
 
-    public static int GetScore()
+    public override void _ExitTree()
+    {
+        SaveScoreToFile();
+    }
+
+
+    public static uint GetScore()
     {
         return Instance._score;
     }
 
-    public static int GetHighScore()
+    public static uint GetHighScore()
     {
         return Instance._highScore;
     }
 
-    public static void SetScore(int value)
+    public static void SetScore(uint value)
     {
         Instance._score = value;
         if (Instance._score > Instance._highScore)
@@ -39,7 +51,29 @@ public partial class ScoreManager : Node
 
     public static void IncrimentScore()
     {
-        SetScore(GetScore() + 1); 
+        SetScore(GetScore() + 1);
     }
 
+    private void SaveScoreToFile()
+    {
+        using FileAccess file = FileAccess.Open(SCORE_FILE, FileAccess.ModeFlags.Write);
+        if (file != null)
+        {
+            file.Store32(_highScore);
+        }
+    }
+
+    private void LoadScoreFromFile()
+    {
+        using FileAccess file = FileAccess.Open(SCORE_FILE, FileAccess.ModeFlags.Read);
+        if (file != null)
+        {
+            _highScore = file.Get32();
+        }
+        else
+        {
+            _highScore = 0;
+        } 
+            
+    }
 }
--- Scenes/Main/Main.cs
+++ Scenes/Main/Main.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class Main : Control
 {
+	[Export] private Label _highScoreLabel;
 
 	private static readonly PackedScene GAME_SCENE =
 				GD.Load<PackedScene>("res://Scenes/Game/Game.tscn");
@@ -10,7 +11,9 @@ public partial class Main : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_highScoreLabel.Text = $"{ScoreManager.GetHighScore():0000}";
 	}
+
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
